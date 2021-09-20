be8a505cfd573f735f5e607e72516176b03ea26b
WinterSolid <58896705+WinterSolid@users.noreply.github.com>
2021-09-20 16:21:51 -0500

    API call - work in progress
    Working on API call logic (SearchManager class)
    TODO:
    Dictionaries
    protocols
    URL extension

--- Book By Cover/Book By Cover/SearchManager.swift
+++ Book By Cover/Book By Cover/SearchManager.swift
@@ -3,10 +3,27 @@
 //  Book By Cover
 //
 //  Created by Zakee Tanksley on 9/20/21.
-//
+// https://www.googleapis.com/books/v1/volumes?q=search+terms
 
 import Foundation
 
-class SearchMAnager {
+class SearchManager {
     
+    func getBookInfo(isbn: String, completion: @escaping (Books)-> Void) {
+        
+        let sessionConfig = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        
+        guard var URL = URL(string: "https://www.googleapis.com/books/v1/volumes?q=search+terms")
+        else {return}
+        
+        let URLParams = [ "q": "isbn: \(isbn)"]
+        URL
+    }
+    URL = URL.appendingQueryParameters(URLParams)
+    var request = URLRequest(url: URL)
+    request.httpMethod = "GET"
+    
+    let task = session.dataTask() //TODO
 }
